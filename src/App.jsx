d01d0f9c07361e38b2b609c1375ff6c10694a311
firatcanulukaya <mua.firat@gmail.com>
2022-07-13 18:59:13 +0300

    changed colons

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import React from "react";
+import React from 'react';
 import Navbar from 'components/Navbar/Navbar';
 import Landing from 'components/Landing/Landing';
-import AboutUs from "components/AboutUs/AboutUs";
-import Team from "components/Team/Team";
-import Footer from "components/Footer/Footer";
+import AboutUs from 'components/AboutUs/AboutUs';
+import Team from 'components/Team/Team';
+import Footer from 'components/Footer/Footer';
 
 const App = () => {
     return (
